92b592abfd76992259626e9ee040a98a2449a28d
Juanma Barea Martinez <juanmabareamartinez@gmail.com>
2024-02-13 11:53:02 +0100

    Update argocd event subscriptor image version

--- argocd-event-subscriptor/src/main/java/org/jmb/SubscriptorResource.java
+++ argocd-event-subscriptor/src/main/java/org/jmb/SubscriptorResource.java
@@ -1,6 +1,7 @@
 package org.jmb;
 
 import io.quarkus.logging.Log;
+import jakarta.json.Json;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
@@ -10,8 +11,8 @@ import jakarta.ws.rs.core.MediaType;
 public class SubscriptorResource {
     
     @POST
-    //@Consumes(MediaType.APPLICATION_JSON)
-    public void receiveEvent(String event) {
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void receiveEvent(Json event) {
         Log.info(event);
     }
 }
