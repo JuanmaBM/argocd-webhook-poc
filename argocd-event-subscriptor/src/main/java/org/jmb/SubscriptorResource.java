d5ac8195c79ec0948ce6dd6e524de680d11e0100
Juanma Barea Martinez <juanmabareamartinez@gmail.com>
2024-02-13 13:02:02 +0100

    Return to quarkus subscriptor version

--- argocd-event-subscriptor/src/main/java/org/jmb/SubscriptorResource.java
+++ argocd-event-subscriptor/src/main/java/org/jmb/SubscriptorResource.java
@@ -6,35 +6,12 @@ import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.MediaType;
 
-import java.util.Map;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @Path("/")
 public class SubscriptorResource {
     
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
+    //@Consumes(MediaType.APPLICATION_JSON)
     public void receiveEvent(String event) {
-
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        try {
-            JsonNode jsonNode = objectMapper.readTree(event);
-            Map<String, Object> app = objectMapper.convertValue(jsonNode.get("app"), Map.class);
-            Map<String, Object> metadata = objectMapper.convertValue(jsonNode.get("metadata"), Map.class);
-            Map<String, Object> status = objectMapper.convertValue(jsonNode.get("status"), Map.class);
-
-            Log.info("app: ");
-            Log.info(app);
-            Log.info("metadata: ");
-            Log.info(metadata);
-            Log.info("status: ");
-            Log.info(status);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
+        Log.info(event);
     }
 }
